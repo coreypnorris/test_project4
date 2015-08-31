2e1b88857c3b31a0e71702286796e7074330fa99
Corey Norris <coreypnorris@gmail.com>
2015-08-30 20:43:39 -0700

    install bourbon and neat

--- PublicComics.Web/Batch_Files/compile-sass-files.bat
+++ PublicComics.Web/Batch_Files/compile-sass-files.bat
@@ -1,1 +1,2 @@
-node-sass Custom_Scss\global-base.scss >> Custom_Css\global-base.css
+node-sass Custom_Scss\global-base.scss >> Custom_Css\global-base.css && ^
+node-sass Custom_Scss\home-index.scss >> Custom_Css\home-index.css
